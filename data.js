2d376b1f4d79e6688333c880255096ab869167bc
glgaines <glgaines@users.noreply.github.com>
2017-08-31 19:15:55 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,21 +1,37 @@
 {
-	"query": {"visits":[	
-			{"date": "08/20/2017",
-			"time": "10:30 AM",
-			"reason": "general check-up",
-			"doctor": "Yousef",
-				"location": "123 South Street, Scotia, NY",
-				"transportation": "self-drive",
-				"notes": ["remember med list", "urine sample"]
-			},		
-			{"date": "08/20/2017",
-				"time": "10:30 AM",
-				"reason": "general check-up with medicines",
-				"doctor": "Yousef",
-				"location": "123 South Street, Scotia, NY",
-				"transportation": "self-drive",
-				"notes": ["remember med list", "urine sample"]
-			}
-		]
-	}
+  "query": {
+    "searchinfo": {
+      "totalhits": 4768
+    },
+    "search": [
+      {
+        "ns": 0,
+        "title": "Albert Einstein",
+        "pageid": 736,
+        "size": 144068,
+        "wordcount": 15119,
+        "snippet": "<span class=\"searchmatch\">Albert</span> <span class=\"searchmatch\">Einstein</span> (14 March 1879 – 18 April 1955) was a German-born theoretical physicist. <span class=\"searchmatch\">Einstein</span> developed the theory of relativity, one of the two pillars",
+        "timestamp": "2017-08-30T23:33:11Z"
+      },
+      {
+        "ns": 0,
+        "title": "Einstein family",
+        "pageid": 18742711,
+        "size": 37264,
+        "wordcount": 3616,
+        "snippet": "Variant (of 2): Ainstein.    The <span class=\"searchmatch\">Einstein</span> family is the family of the renowned physicist <span class=\"searchmatch\">Albert</span> <span class=\"searchmatch\">Einstein</span> (1879–1955). <span class=\"searchmatch\">Einstein</span>'s great-great-great-great-grandfather",
+        "timestamp": "2017-08-30T00:07:37Z"
+      },
+      {
+        "ns": 0,
+        "title": "Albert Einstein College of Medicine",
+        "pageid": 319759,
+        "size": 55311,
+        "wordcount": 5594,
+        "snippet": "﻿40.850852°N 73.844949°W﻿ / 40.850852; -73.844949 The <span class=\"searchmatch\">Albert</span> <span class=\"searchmatch\">Einstein</span> College of Medicine (&quot;<span class=\"searchmatch\">Einstein</span>&quot; for short), a joint entity between Montefiore Medical",
+        "timestamp": "2017-08-29T21:53:35Z"
+      }
+      ]
+  }
 }
+  
