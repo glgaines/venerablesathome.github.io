8b2ab090f5b8e2089e94de518824b8c2fc689cc3
glgaines <glgaines@users.noreply.github.com>
2017-08-31 14:04:06 -0400

    Create data.js

---
+++ data.js
@@ -0,0 +1,34 @@
+[
+	{	date: '08/20/2017',
+		time: '10:30 AM',
+		reason: 'general check-up',
+		doctor: 'Yousef',
+		location: '123 South Street, Scotia, NY',
+		transportation: 'self-drive',
+		notes: ['remember med list', 'urine sample']
+	},
+	{	date: '08/30/2017',
+		time: '10:30 AM',
+		reason: 'Cardiologist - checking med levels',
+		doctor: 'Card',
+		location: '456 North Street, Scotia, NY',
+		transportation: 'self-drive',
+		notes: ['remember med list', 'discuss being tired']
+	},
+	{	date: '09/02/2017',
+		time: '2:30 PM',
+		reason: 'first with ENT',
+		doctor: 'Noser',
+		location: '894 West Street, Scotia, NY',
+		transportation: 'self-drive',
+		notes: ['remember med list', 'urine sample']
+	},
+	{	date: '09/20/2017',
+		time: '8:30 AM',
+		reason: 'Endoscopy',
+		doctor: 'Bowels',
+		location: 'Ellis Hospital',
+		transportation: 'Need Transport',
+		notes: ['remember med list', 'No eat, drink for 24 hours']
+	},
+]
