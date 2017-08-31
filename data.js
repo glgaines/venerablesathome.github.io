56cf4585d85a8f13f819b4c1e7908d29fb88f5a3
glgaines <glgaines@users.noreply.github.com>
2017-08-31 19:06:58 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,20 +1,20 @@
 {
 	"query": {"visits": [	
-			{	'date': '08/20/2017',
-				'time': '10:30 AM',
-				'reason': 'general check-up',
-				'doctor': 'Yousef',
-				'location': '123 South Street, Scotia, NY',
-				'transportation': 'self-drive',
-				'notes': ['remember med list', 'urine sample']
+			{	"date": "08/20/2017",
+				"time": "10:30 AM",
+				"reason": "general check-up",
+				"doctor": "Yousef",
+				"location": "123 South Street, Scotia, NY",
+				"transportation": "self-drive",
+				"notes": ["remember med list", "urine sample"]
 			},		
-			{	'date': '08/20/2017',
-				'time': '10:30 AM',
-				'reason': 'general check-up with medicines',
-				'doctor': 'Yousef',
-				'location': '123 South Street, Scotia, NY',
-				'transportation': 'self-drive',
-				'notes': ['remember med list', 'urine sample']
+			{	"date": "08/20/2017",
+				"time": "10:30 AM",
+				"reason": "general check-up with medicines",
+				"doctor": "Yousef",
+				"location": "123 South Street, Scotia, NY",
+				"transportation": "self-drive",
+				"notes": ["remember med list", "urine sample"]
 			}
 		]
 	}
