8b52dc7e45e914bb9843246ccf9343fd2725c693
glgaines <glgaines@users.noreply.github.com>
2017-09-01 15:18:19 -0400

    Update data.js

--- data.js
+++ data.js
@@ -26,7 +26,31 @@
 				"location": "123 South Street, Scotia, NY",
 				"transportation": "self-drive",
 				"notes": "a med list and a urine sample"
-			}
+			},
+	    {	"date": "08/30/2017",
+		"time": "10:30 AM",
+		"reason": "Cardiologist - checking med levels",
+		"doctor": "Card",
+		"location": "456 North Street, Scotia, NY",
+		"transportation": "self-drive",
+		"notes": "remember med list and to discuss being tired"
+	},
+	{	"date": "09/02/2017",
+		"time": "2:30 PM",
+		"reason": "first with ophthalmologist",
+		"doctor": "Noser",
+		"location": "894 West Street, Scotia, NY",
+		"transportation": "self-drive",
+		"notes": "med list and your eye drops.  Also the trouble with reading"
+	},
+	{	"date": "09/20/2017",
+		"time": "8:30 AM",
+		"reason": "Endoscopy",
+		"doctor": "Bowels",
+		"location": "Ellis Hospital",
+		"transportation": "Need Transport",
+		"notes": "remember med list and Not eat, drink for 24 hours"
+	}
       ]
   }
 }
