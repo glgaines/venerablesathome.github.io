6675bcf6d037d48ba2637511130e3ecde8c6009d
glgaines <glgaines@users.noreply.github.com>
2017-09-01 15:20:22 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,24 +1,6 @@
 {
   "query": {
     "visits": [
-      {
-        "ns": 0,
-        "title": "Albert Einstein",
-        "pageid": 736,
-        "size": 144068,
-        "wordcount": 15119,
-        "snippet": "<span class=\"searchmatch\">Albert</span> <span class=\"searchmatch\">Einstein</span> (14 March 1879 – 18 April 1955) was a German-born theoretical physicist. <span class=\"searchmatch\">Einstein</span> developed the theory of relativity, one of the two pillars",
-        "timestamp": "2017-08-30T23:33:11Z"
-      },
-      {
-        "ns": 0,
-        "title": "Einstein family",
-        "pageid": 18742711,
-        "size": 37264,
-        "wordcount": 3616,
-        "snippet": "Variant (of 2): Ainstein.    The <span class=\"searchmatch\">Einstein</span> family is the family of the renowned physicist <span class=\"searchmatch\">Albert</span> <span class=\"searchmatch\">Einstein</span> (1879–1955). <span class=\"searchmatch\">Einstein</span>'s great-great-great-great-grandfather",
-        "timestamp": "2017-08-30T00:07:37Z"
-      },
       {	"date": "08/20/2017",
 				"time": "10:30 AM",
 				"reason": "general check-up",
