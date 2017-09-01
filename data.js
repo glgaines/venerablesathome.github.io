399fd6f34489aff66e7854c655cbfc72bb7d1300
glgaines <glgaines@users.noreply.github.com>
2017-09-01 13:51:31 -0400

    Update data.js

--- data.js
+++ data.js
@@ -22,7 +22,7 @@
       {	"date": "08/20/2017",
 				"time": "10:30 AM",
 				"reason": "general check-up",
-				"doctor": "Smith",
+				"doctor": "Yousef",
 				"location": "123 South Street, Scotia, NY",
 				"transportation": "self-drive",
 				"notes": "a med list and a urine sample"
